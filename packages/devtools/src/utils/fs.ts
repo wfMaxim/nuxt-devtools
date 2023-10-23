3f1260e5a36da269589f6d46e2e069ce089fcf26
Maxim <info@wallfar.com>
2023-10-23 16:14:01 +0200

    new v

--- packages/devtools/src/utils/fs.ts
+++ packages/devtools/src/utils/fs.ts
@@ -1,6 +1,10 @@
 import type { Dirent } from 'node:fs'
 import { lstat, readdir } from 'node:fs/promises'
+import { promises as fs } from 'node:fs'
+import path from 'node:path'
 import { resolve } from 'pathe'
+
+import { parse } from '@vue/compiler-sfc'
 
 export async function getFolderSize(
   dir: string,
@@ -41,29 +45,104 @@ export async function getFolderSize(
   return sizes.reduce((total, size) => (total += size), 0)
 }
 
-export async function getComponentProps(componentPath: string) {
-  let componentOptions
-
+export async function getComponentProps(componentPath) {
   try {
-    const component = await import(`${componentPath}`)
-    componentOptions = component.default || component
+    const filePath = path.resolve(__dirname, componentPath)
+    const content = await fs.readFile(filePath, 'utf-8')
+    const { descriptor, errors } = parse(content)
+
+    if (errors && errors.length > 0) {
+      /* eslint-disable-next-line no-console */
+      console.error('Errors while parsing the Vue file:', errors)
+      return {}
+    }
+
+    if (!descriptor) {
+      /* eslint-disable-next-line no-console */
+      console.error('Descriptor is undefined. The Vue file may not be correctly formatted.')
+      return {}
+    }
+
+    if (descriptor.scriptSetup) {
+      const scriptContent = descriptor.scriptSetup.content
+      // Here you can use scriptContent to get your component props
+      // You can either parse the scriptContent manually or try to execute it in a safe environment
+      /* eslint-disable-next-line no-console */
+      console.log('Script Setup Content:', await extractPropsFromScript(scriptContent))
+    }
+
+    if (descriptor.script) {
+      const scriptContent = descriptor.script.content
+      // Here you can use scriptContent to get your component props
+      // You can either parse the scriptContent manually or try to execute it in a safe environment
+      /* eslint-disable-next-line no-console */
+      console.log('Script Content:', await extractPropsFromScript(scriptContent))
+    }
   }
   catch (error) {
+    /* eslint-disable-next-line no-console */
     console.error(`Error while importing component: ${error}`)
     return {}
   }
+}
 
-  const props = componentOptions.props || {}
-  const propDetails = {}
+async function extractPropsFromScript(scriptContent) {
+  const isCompositionAPI = scriptContent.includes('defineProps') || scriptContent.includes('<script setup>')
 
-  Object.keys(props).forEach((key) => {
-    const prop = props[key]
-    propDetails[key] = {
-      type: prop.type ? prop.type.name || prop.type.toString() : 'Unknown',
-      required: prop.required || false,
-      default: prop.default,
+  if (isCompositionAPI)
+    return extractPropsFromCompositionAPI(scriptContent)
+  else
+    return extractPropsFromOptionsAPI(scriptContent)
+};
+
+async function extractPropsFromCompositionAPI(scriptContent) {
+  try {
+    const propsRegex = /defineProps\(([\s\S]*?)\)/
+    const match = scriptContent.match(propsRegex)
+
+    if (match) {
+      const propsContent = match[1].trim()
+      // TODO: You would need to parse propsContent here to actually extract the prop types.
+      // This could be complex depending on how props are defined and might require a full JS parser.
+      return propsContent
     }
-  })
+    else {
+      /* eslint-disable-next-line no-console */
+      console.log('No props found')
+    }
+  }
+  catch (error) {
+    /* eslint-disable-next-line no-console */
+    console.log(error)
+  }
+};
 
-  return propDetails
+function extractPropsFromOptionsAPI(scriptContent) {
+  // First, you need to isolate the export default object
+  const exportDefaultRegex = /export\s+default\s+({[\s\S]*})/
+  const match = scriptContent.match(exportDefaultRegex)
+
+  if (match) {
+    const exportedObjectContent = match[1].trim()
+
+    // Now, find the props property inside the exported object
+    // This regex is very simplistic and assumes that props are defined as an object literal
+    const propsRegex = /props:\s*({[\s\S]*})/
+    const propsMatch = exportedObjectContent.match(propsRegex)
+
+    if (propsMatch) {
+      const propsContent = propsMatch[1].trim()
+      // TODO: You would need to parse propsContent here to actually extract the prop definitions.
+      // This could be complex depending on how props are defined and might require a full JS parser.
+      return propsContent
+    }
+    else {
+      /* eslint-disable-next-line no-console */
+      console.log('No props found in exported object')
+    }
+  }
+  else {
+    /* eslint-disable-next-line no-console */
+    console.log('No export default statement found')
+  }
 }
